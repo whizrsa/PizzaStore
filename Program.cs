e2c7619244617d3206f0a90232e1b43515d8d6d9
Elvis Chimuse <elvischimuse@gmail.com>
2025-04-15 03:07:32 +0200

    Inital Pizza Store Api commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.OpenApi.Models;
+using PizzaStore.Db;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+   app.UseSwagger();
+   app.UseSwaggerUI(c =>
+   {
+      c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+   });
+}
+
+app.MapGet("/pizzas/{id}", (int id) => PizzaDb.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDb.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDb.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDb.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDb.RemovePizza(id));
+
+app.Run();
