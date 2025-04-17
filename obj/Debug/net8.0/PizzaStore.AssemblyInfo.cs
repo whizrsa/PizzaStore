a0e130491d00e948f3c8bcc3996cf9862abafa98
Elvis Chimuse <elvischimuse@gmail.com>
2025-04-17 02:34:55 +0200

    Update Pizza

--- obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2c7619244617d3206f0a90232e1b43515d8d6d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
